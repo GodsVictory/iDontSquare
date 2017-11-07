db3901aca8ecac563e90fca603949c581a86f35f
GodsVictory <thegodsvictory@gmail.com>
2017-11-07 15:31:43 -0600

    SingleShape

--- public/js/game.js
+++ public/js/game.js
@@ -1,6 +1,6 @@
 var app;
 var socket;
-var shapes = [];
+var shape;
 
 window.onload = function start() {
   PIXI.SCALE_MODES.DEFAULT = PIXI.SCALE_MODES.NEAREST;
@@ -9,5 +9,41 @@ window.onload = function start() {
     antialias: true
   });
   document.getElementById('game').appendChild(app.view);
-  openSocket();
+
+  socket = io();
+  socket.on('initialize', function(data) {
+    document.getElementById('name').value = data.id.substr(0, 5);
+    printScore(data.score);
+    newShape(data.shape.x, data.shape.y, data.shape.size);
+  });
+  socket.on('score', function(data) {
+    printScore(data);
+  });
+  socket.on('shape', function(shape) {
+    newShape(shape.x, shape.y, shape.size);
+  });
+};
+
+function printScore(data) {
+  document.getElementById('score').innerHTML = "";
+  for (var i = 0, len = data.length; i < len; i++) {
+    var name = document.createElement('p');
+    name.innerHTML = data[i].name + ":&nbsp;&nbsp;&nbsp;" + data[i].score;
+    document.getElementById('score').appendChild(name);
+  }
+}
+
+function newShape(x, y, size) {
+  if (shape)
+    shape.destroy();
+  shape = new PIXI.Graphics();
+  shape.beginFill(0x000000, 1);
+  shape.drawRect(x, y, size, size);
+  app.stage.addChild(shape);
+  shape.interactive = true;
+  shape.on('pointerdown', function() {
+    socket.emit('hit', {
+      name: document.getElementById('name').value
+    });
+  });
 }
--- server.js
+++ server.js
@@ -9,36 +9,31 @@ var port = process.env.PORT || 80;
 server.listen(port);
 
 var io = require('socket.io')(server);
-var players = {};
 var score = [];
-var shapes = [];
+var shape;
 var shapeSize = 50;
+
+var shape = {
+  x: getRandomInt(shapeSize, 800 - shapeSize),
+  y: getRandomInt(shapeSize, 600 - shapeSize),
+  size: shapeSize
+};
+io.emit('shape', shape);
 
 io.on('connection', function(socket) {
   socket.emit('initialize', {
     id: socket.id,
     score: score,
-    shapes: shapes
+    shape: shape
   });
 
   socket.on('hit', function(data) {
-    for (var i = 0, len = shapes.length; i < len; i++) {
-      if (shapes[i].id == data.id) {
-        io.emit('destroyShape', shapes[i].id);
-        shapes.splice(i, 1);
-        break;
-      }
-    }
-
-    var newShape = {
-      id: count,
+    var shape = {
       x: getRandomInt(shapeSize, 800 - shapeSize),
       y: getRandomInt(shapeSize, 600 - shapeSize),
       size: shapeSize
     };
-    shapes.push(newShape)
-    io.emit('shape', newShape);
-    count++;
+    io.emit('shape', shape);
 
     var found = false;
     for (var i = 0, len = score.length; i < len; i++) {
@@ -66,33 +61,6 @@ io.on('connection', function(socket) {
   });
 });
 
-var count = 0;
-var newShape = {
-  id: count,
-  x: getRandomInt(shapeSize, 800 - shapeSize),
-  y: getRandomInt(shapeSize, 600 - shapeSize),
-  size: shapeSize
-};
-shapes.push(newShape)
-io.emit('shape', newShape);
-count++;
-// var tps = 2;
-// const gameloop = require('node-gameloop');
-// const id = gameloop.setGameLoop(function(deltaTime) {
-//   var newShape = {
-//     id: count,
-//     x: getRandomInt(shapeSize, 800 - shapeSize),
-//     y: getRandomInt(shapeSize, 600 - shapeSize),
-//     size: shapeSize
-//   };
-//   shapes.push(newShape)
-//   io.emit('shape', newShape);
-//   count++;
-//   if (shapes.length > 5) {
-//     var remove = shapes.shift();
-//     io.emit('destroyShape', remove.id);
-//   }
-// }, 1000 / tps);
 
 function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
